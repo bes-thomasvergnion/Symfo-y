60f3b2a304765c1c132e258e8b6c76fdf0640a51
Thomas <thomasvergnion@hotmail.com>
2017-02-06 14:47:05 +0100

    test commit

--- src/TV/FindyourbandBundle/Controller/FilterController.php
+++ src/TV/FindyourbandBundle/Controller/FilterController.php
@@ -10,7 +10,7 @@ use TV\FindyourbandBundle\Form\SearchBandType;
 
 class FilterController extends Controller
 {
-    public function indexAdvertAction(Request $request)
+    public function indexAdvertAction()
     {
         $form = $this->get('form.factory')->create(SearchType::class, null, ['action'=>$this->generateUrl('tv_findyourband_filter_select_advert',[], \Symfony\Component\Routing\Router::ABSOLUTE_URL)]);
 
@@ -19,11 +19,11 @@ class FilterController extends Controller
         ));
     }
     
-    public function selectAdvertAction(Request $request)
+    public function selectAdvertAction(Request $request, $page)
     {
         $search = new Search();
         $form = $this->createForm(SearchType::class, $search);
-     
+        $nbPerPage = 5;
        
         if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
             $em = $this->getDoctrine()->getManager();
@@ -34,15 +34,20 @@ class FilterController extends Controller
         $listAdverts = $this->getDoctrine()
             ->getManager()
             ->getRepository('TVFindyourbandBundle:Advert')
-            ->getAdvertsWithFilters($search)
+            ->getAdvertsWithFilters($search, $page, $nbPerPage)
         ;
+        $nbPages = ceil(count($listAdverts)/$nbPerPage);
         
         return $this->render('TVFindyourbandBundle:Advert:list.html.twig', array(
             'listAdverts' => $listAdverts,
+            'nbPerPage' => $nbPerPage,
+            'nbPages' => $nbPages,
+            'page' => $page,
+            
         ));
     }
     
-    public function indexUserAction(Request $request)
+    public function indexUserAction()
     {
         $form = $this->get('form.factory')->create(SearchType::class, null, ['action'=>$this->generateUrl('tv_findyourband_filter_select_user',[], \Symfony\Component\Routing\Router::ABSOLUTE_URL)]);
 
@@ -51,10 +56,11 @@ class FilterController extends Controller
         ));
     }
     
-    public function selectUserAction(Request $request)
+    public function selectUserAction(Request $request, $page)
     {
         $search = new Search();
         $form = $this->createForm(SearchType::class, $search);
+        $nbPerPage = 10;
         $count = 0;
      
        
@@ -67,16 +73,20 @@ class FilterController extends Controller
         $listUsers = $this->getDoctrine()
             ->getManager()
             ->getRepository('TVUserBundle:User')
-            ->getUsersWithFilters($search)
+            ->getUsersWithFilters($search, $page, $nbPerPage)
         ;
+        $nbPages = ceil(count($listUsers)/$nbPerPage);
         
         return $this->render('TVUserBundle:User:list.html.twig', array(
             'listUsers' => $listUsers,
             'count' => $count,
+            'nbPerPage' => $nbPerPage,
+            'nbPages' => $nbPages,
+            'page' => $page,
         ));
     }
     
-    public function indexBandAction(Request $request)
+    public function indexBandAction()
     {
         $form = $this->get('form.factory')->create(SearchBandType::class, null, ['action'=>$this->generateUrl('tv_findyourband_filter_select_band',[], \Symfony\Component\Routing\Router::ABSOLUTE_URL)]);
 
@@ -85,11 +95,12 @@ class FilterController extends Controller
         ));
     }
     
-    public function selectBandAction(Request $request)
+    public function selectBandAction(Request $request, $page)
     {
         $search = new Search();
         $form = $this->createForm(SearchType::class, $search);    
         $count = 0;
+        $nbPerPage = 20;
         
         if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
             $em = $this->getDoctrine()->getManager();
@@ -100,12 +111,16 @@ class FilterController extends Controller
         $listBands = $this->getDoctrine()
             ->getManager()
             ->getRepository('TVUserBundle:Band')
-            ->getBandsWithFilters($search)
+            ->getBandsWithFilters($search, $page, $nbPerPage)
         ;
+        $nbPages = ceil(count($listBands)/$nbPerPage);
         
         return $this->render('TVUserBundle:Band:list.html.twig', array(
             'listBands' => $listBands,
             'count' => $count,
+            'nbPerPage' => $nbPerPage,
+            'nbPages' => $nbPages,
+            'page' => $page,
         ));
     }
     
